1707f97abeeb6ff6e8960b23bd933bda0b17b7cc
Danil <dinkodanil@gmail.com>
2021-02-12 14:19:07 +0300

    Finish programm

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { clearResult, clearExample, solveExample, addToExample } from '../redux/actions/result'
 
 export default function Keyboard({ dispatch }) {
     return (
@@ -8,41 +9,44 @@ export default function Keyboard({ dispatch }) {
             </div>
 
             <div class="buttons">
-                <div class="button__c">C</div>
-                <div class="button__plmin">
+                <div class="button__c" onClick={() => {
+                    dispatch(clearResult())
+                    dispatch(clearExample())
+                }}>C</div>
+                <div class="button__plmin" onClick={() => dispatch(addToExample("-"))}>
                     <img src="https://localhost:9999/assets/plmin.svg" alt="" />
                 </div>
-                    <div class="button__percent">
+                    <div class="button__percent" onClick={() => dispatch(addToExample("%"))}>
                         <img src="img/percent.svg" alt="" />
                     </div>
-                    <div class="button__act">
+                    <div class="button__act" onClick={() => dispatch(addToExample("/"))}>
                         <img src="https://localhost:9999/assets/split.svg" alt="" />
                     </div>
 
-                    <div class="button__number">7</div>
-                    <div class="button__number">8</div>
-                    <div class="button__number">9</div>
-                    <div class="button__act">
+                    <div class="button__number" onClick={() => dispatch(addToExample("7"))}>7</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("8"))}>8</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("9"))}>9</div>
+                    <div class="button__act" onClick={() => dispatch(addToExample("*"))}>
                         <img src="https://localhost:9999/assets/mult.svg" alt="" />
                     </div>
 
-                    <div class="button__number">4</div>
-                    <div class="button__number">5</div>
-                    <div class="button__number">6</div>
-                    <div class="button__act">
+                    <div class="button__number" onClick={() => dispatch(addToExample("4"))}>4</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("5"))}>5</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("6"))}>6</div>
+                    <div class="button__act" onClick={() => dispatch(addToExample("-"))}>
                         <img src="https://localhost:9999/assets/-.svg" alt="" />
                     </div>
 
-                    <div class="button__number">1</div>
-                    <div class="button__number">2</div>
-                    <div class="button__number">3</div>
-                    <div class="button__act">
+                    <div class="button__number" onClick={() => dispatch(addToExample("1"))}>1</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("2"))}>2</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("3"))}>3</div>
+                    <div class="button__act" onClick={() => dispatch(addToExample("+"))}>
                         <img src="https://localhost:9999/assets/+.svg" alt="" />
                     </div>
                     
-                    <div class="button__null">0</div>
-                    <div class="button__number">.</div>
-                    <div class="button__result">=</div>
+                    <div class="button__null" onClick={() => dispatch(addToExample("0"))}>0</div>
+                    <div class="button__number" onClick={() => dispatch(addToExample("."))}>.</div>
+                    <div class="button__result" onClick={() => dispatch(solveExample())}>=</div>
             </div>
         </div>
     )
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -9,8 +9,8 @@ function Result({ result, example }) {
                 <img src="https://localhost:9999/assets/dark/sun.svg" alt="" class="controller__item" />
             </div>
 
-            <div class="result__sample">120 x 5</div>
-            <div class="result__result">26000</div>
+            <div class="result__sample">{example}</div>
+            <div class="result__result">{result}</div>
                 
         </div>
     )
